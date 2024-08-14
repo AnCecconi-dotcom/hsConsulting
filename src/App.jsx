9539ab6a66b4c244ccac0ae4f0ebb2d498dd2749
AndresCecconi <ancecconi15@gmail.com>
2024-08-14 16:01:29 -0300

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Header } from './components/Header';
 
 export const App = () => {
   return (
     <>
-    <Header />
+    <h1>HS CONSULTING</h1>
     </>
   )
 };
--- src/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { logoHs } from '../assets/logoHsSinFondo.png';
-
-export const Logo = () => {
-  return (
-   <img src={logoHs} alt="HS" />
-  )
-}
