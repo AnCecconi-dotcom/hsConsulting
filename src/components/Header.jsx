8fbb72957370ce096ff296907f994c93893f4343
AndresCecconi <ancecconi15@gmail.com>
2024-08-14 11:22:02 -0300

    creacion repositorio

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar } from './Navbar'
+
+export const Header = () => {
+    return (
+        <div className='container p-0'>
+            <div className="row">
+                <div className='col md-12'>
+                    <Navbar />
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { logoHs } from '../assets/logoHsSinFondo.png';
+
+export const Logo = () => {
+  return (
+   <img src={logoHs} alt="HS" />
+  )
+}
